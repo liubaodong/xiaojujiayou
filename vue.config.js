5e92e7a2ac6595108b27c37d605b29fc3aa7cd77
sir-lbd <2458825436@qq.com>
2020-05-05 10:17:25 +0800

    小桔加油初始化

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({ // 把px单位换算成rem单位
+            rootValue: 37.5, // vant官方使用的是37.5
+            selectorBlackList: ['vant', 'mu'], // 忽略转换正则匹配项
+            propList: ['*']
+          })
+        ]
+      }
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+    }
+  }
+}
