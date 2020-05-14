64073969da3bd1c8293607697dd43311578b3447
sir-lbd <2458825436@qq.com>
2020-05-14 23:51:16 +0800

    第一版

--- src/views/orange-list/index.vue
+++ src/views/orange-list/index.vue
@@ -179,7 +179,7 @@ export default {
       const _this = this;
       const wx = this.$wx;
       wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: object.appId, // 必填，公众号的唯一标识
         timestamp: object.timestamp, // 必填，生成签名的时间戳
         nonceStr: object.nonceStr, // 必填，生成签名的随机串
